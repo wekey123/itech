d131142bbbf1fbc4b3f2ae56650d01ca6d2b0d86
vignesh <mail2rmvignesh@gmail.com>
2016-02-24 19:08:17 +0530

    Added mobile service page and the images

---
+++ mobileservices.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Services</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+	<?php include 'elements/header_include.php'; ?>
+</head>
+<body>
+<div class="page">
+<!--========================================================
+                              HEADER
+    =========================================================-->
+     <?php include 'elements/header.php'; ?>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main style="border-bottom:1px solid #ccc">
+        <section class="well-1 well-1__off-1 divider-xs well-xs-1">
+            <div class="container">
+                <h3><strong>Mobile</strong> Application services</h3>
+
+                <div class="row row__off-1 grid-off-lg">
+   
+                    
+                    <div class="col-md-6 col-sm-12 wow fadeInRight" data-wow-delay="0.2s">
+                        <div class="icon-block icon-block__lg  fa-apple">
+                            <h5>
+                                <a href='#'>iPhone Application</a>
+                            </h5>
+                            <p>Itechmatics design & development team has been creating iPhone applications since the App store first opened, we have experience in building Insurance, Banking, Health Care, Retail and Travel Applications.</p> 
+<p>Working through each phase of development from defining a scope of work to rigorous testing and app deployment, ITechmatics project management is well versed in making sure that our projects finish on time, in budget and within scope.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 wow fadeInRight" data-wow-delay="0.4s">
+                        <div class="icon-block icon-block__lg fa-android ">
+                            <h5>
+                                <a href='#'>Android Application</a>
+                            </h5>
+                            <p>Itechmatics has a dedicated Android development team for creating innovative apps on Android platform.</p>
+                            <p>The technical expertise of our team enables us to deliver applications with rich UI quality and better performance. Our team consists of both skilled designers and developers who help us in designing, developing, delivering and launching the app within short duration. Our Android developers have experience and expertise to help you with all Android development needs.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="parallax" data-url="images/parallax1.jpg" data-mobile="true" data-speed="1">
+
+            <div class="bg-secondary well-5" style="padding: 10px 0 20px;">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-md-7 col-off-xs wow fadeInLeft" data-wow-delay="0.2s">
+                            <h2 class="title-wrap  fa-mobile"><strong>Why</strong> Mobile Applications?</h2>
+                        </div>
+                        <div class="col-md-5 col-media wow fadeInRight" data-wow-delay="0.2s">
+                            <p>
+                            <ul class="marked-list offset-1">
+                           	 <li>
+                              Direct channel to reach customers.
+                              </li>
+                               <li>
+                              Developing a direct relationship with the consumers.
+                             </li>
+                             <li>
+                              Mobile capability provides the enterprises leader
+in the market.
+                             </li>
+                             <li>
+iPhone Applications are increasingly a vital
+component of brand marketers' overall strategy,
+creating a rush to develop and deliver content and
+advertising to customers via their mobile devices.
+                             </li>
+                             <li>
+                             Businesses can make their business accessibility
+better by providing the end users to interact using their
+mobile applications.
+                             </li>
+                          </ul>
+						  </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="well-4 well-4__off">
+            <div class="container">
+                <h3><strong>Mobile</strong> Portfolio</h3>
+
+                <div class="row row__off-1">
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.2s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_banking.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='#'>Mobile Banking</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">Itechmatics Inc is one of the niche banking software
+companies which give financial institutions a
+competitive edge by integrating state-of-the-art
+technology with a multitude of pioneering mobile
+marketing and advertising features.
+The outcome is a secure and scalable platform that helps
+your organization to increase loyalty, acquire new
+customers, retain the existing customers, cross-sell
+products plus much more.</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.4s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_health.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='#'>Health Care</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">
+						Our Health Care Mobile App is another way of
+helping your customers to improve their health and
+well-being, so we want you to have the simplest,
+easiest & the most innovative mobile Health App for
+your Customers!!
+Be it the Health Insurance plan coverage details,
+wellness information, checking of claims status,
+access ID card info, finding a doctor or hospital or
+emergency care centers and pharmacies,
+Comprehensive record-keeping of your personal
+medical information, our Health Care Mobile App
+can execute instantly!!
+						</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.6s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_travel.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='#'>Travel & Hospitality (Flight, Car, Hotel)</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">
+						The recent research shows that 80 percent of mobile
+hotel bookings are for stays for that night only. For
+example when traveler opens the app, it assumes
+that the traveler is searching for a hotel in the
+location where the traveler is currently in and
+delivers availability and rate on the search screen.
+Optimized travel app for mobile channel, the travel
+app is with you wherever you go. The app
+incorporates flight search, car & hotel booking,
+Flight Tracker and Save My Trips, so you can simply
+manage your itinerary.
+						</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="parallax" data-url="images/parallax1.jpg" data-mobile="true" data-speed="1">
+            <div class="bg-primary well-5 well-5__off">
+
+                <div class="container wow fadeInUp">
+                    <h2><strong>Our </strong>Recommendation to your Business</h2>
+
+                    <p class="offset">We recommend that you view your mobile strategy as a
+movement that can be transformational to your project,
+Company or Organization. It is about discovering how to
+infuse a new medium into your business and build an
+innovative platform that will take you not just into the
+next year, but into the next decade. For example:
+For a sales-based organization, you could use the mobile
+web to get information to your sales people in the field,
+allowing them to bring up live pricing and estimates
+while they are at lunch with a Client.
+For health care, you could use mobile devices to provide
+records; access to formularies, policies, and procedures;
+even patient charts—and all at the patient’s bedside.
+					</p>
+                    <a href="#" class="btn-primary btn-primary__md btn-off">Learn more</a>
+                </div>
+
+            </div>
+        </section>
+        <section class="well-4 well-4__off">
+            <div class="container">
+                <h3><strong>Mobile Application </strong>Supports</h3>
+
+                <div class="row row__off-1">
+                    <div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                            <li>
+                                <a href='#'>Mobile Insurance
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Banking</a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Media & Publishing</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                            <li>
+                                <a href='#'>Mobile Retail,Coupons
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Travel & Tickets</a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Billing & Payments</a>
+                            </li>
+                        </ul>
+                    </div>
+					<div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                            <li>
+                                <a href='#'>Mobile Reading & learning
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Social & Marketing</a>
+                            </li>
+                            <li>
+                                <a href='#'>Mobile Tracking & Reports</a>
+                            </li>
+                        </ul>
+                    </div>
+					
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <?php include 'elements/footer.php'; ?>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
