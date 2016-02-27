d43b24ff090cbe2ee899d276bb66b6d81336905b
vignesh <mail2rmvignesh@gmail.com>
2016-02-27 18:22:56 +0530

    ecommerce page added and updates in the Home page.

---
+++ ecommerce.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Services</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+	<?php include 'elements/header_include.php'; ?>
+</head>
+<body>
+<div class="page">
+<!--========================================================
+                              HEADER
+    =========================================================-->
+     <?php include 'elements/header.php'; ?>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main style="border-bottom:1px solid #ccc">
+        <section class="well-1 well-1__off-1 divider-xs well-xs-1">
+            <div class="container">
+                <h3><strong>E-Commerce & Web</strong> Application services</h3>
+
+                <div class="row row__off-1 grid-off-lg">
+   
+                    
+                    <div class="col-md-6 col-sm-12 wow fadeInRight" data-wow-delay="0.2s">
+                        <div class="icon-block icon-block__lg  fa-shopping-cart">
+                            <h5>
+                                <a href='javascript:void(0);'>E-commerce Application</a>
+                            </h5>
+                            <p>E-commerce (electronic commerce or EC) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the Internet.</p><p>These business transactions occur either business-to-business, business-to-consumer, consumer-to-consumer or consumer-to-business. The terms e-commerceand e-business are often used interchangeably. The term e-tail is also sometimes used in reference to transactional processes around online retail.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 wow fadeInRight" data-wow-delay="0.4s">
+                        <div class="icon-block icon-block__lg fa-connectdevelop ">
+                            <h5>
+                                <a href='javascript:void(0);'>Web Development</a>
+                            </h5>
+                            <p>Web development can range from developing the simplest static single page of plain text to the most complex web-based internet applications, electronic businesses, and social network services. </p>
+<p>A more comprehensive list of tasks to which web development commonly refers, may include web design, web content development, client-side/server-side scripting, web server and network security configuration, and e-commerce development.
+</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="parallax" data-url="images/parallax1.jpg" data-mobile="true" data-speed="1">
+
+            <div class="bg-secondary well-5" style="padding: 10px 0 20px;">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-md-7 col-off-xs wow fadeInLeft" data-wow-delay="0.2s">
+                            <h2 class="title-wrap  fa-mobile"><strong>Why </strong>E-Commerce & Web Site ?</h2>
+                        </div>
+                        <div class="col-md-5 col-media wow fadeInRight" data-wow-delay="0.2s">
+                            <p>
+                            <ul class="marked-list offset-1">
+                           	 <li>
+                             	The information is available to be accessed by anyone, anywhere, anytime.
+                              </li>
+                               <li>
+                                The site can be updated to always provide the latest information.
+                             </li>
+                             <li>
+                             	 There is no need to carry a hard copy of information with you. 
+                             </li>
+                             <li>
+								 Easy to use the information to create your own resource.
+                             </li>
+                          </ul>
+						  </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="well-4 well-4__off">
+            <div class="container">
+                <h3><strong>Mobile</strong> Portfolio</h3>
+
+                <div class="row row__off-1">
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.2s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_banking.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='javascript:void(0);'>Mobile Banking</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">Itechmatics Inc is one of the niche banking software
+companies which give financial institutions a
+competitive edge by integrating state-of-the-art
+technology with a multitude of pioneering mobile
+marketing and advertising features.
+The outcome is a secure and scalable platform that helps
+your organization to increase loyalty, acquire new
+customers, retain the existing customers, cross-sell
+products plus much more.</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.4s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_health.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='javascript:void(0);'>Health Care</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">
+						Our Health Care Mobile App is another way of
+helping your customers to improve their health and
+well-being, so we want you to have the simplest,
+easiest & the most innovative mobile Health App for
+your Customers!!
+Be it the Health Insurance plan coverage details,
+wellness information, checking of claims status,
+access ID card info, finding a doctor or hospital or
+emergency care centers and pharmacies,
+Comprehensive record-keeping of your personal
+medical information, our Health Care Mobile App
+can execute instantly!!
+						</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+                    <div class="col-md-4 col-sm-12 wow fadeInRight" data-wow-delay="0.6s">
+                        <div class="center-media-sm offset">
+                            <img src="images/mobile_travel.png" alt=""/>
+                        </div>
+                        <h5>
+                            <a href='javascript:void(0);'>Travel & Hospitality (Flight, Car, Hotel)</a>
+                        </h5>
+                        <p class="offset" style="text-align:justify">
+						The recent research shows that 80 percent of mobile
+hotel bookings are for stays for that night only. For
+example when traveler opens the app, it assumes
+that the traveler is searching for a hotel in the
+location where the traveler is currently in and
+delivers availability and rate on the search screen.
+Optimized travel app for mobile channel, the travel
+app is with you wherever you go. The app
+incorporates flight search, car & hotel booking,
+Flight Tracker and Save My Trips, so you can simply
+manage your itinerary.
+						</p>
+                        <!--<a href="#" class="btn-primary">Learn more</a>-->
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="parallax" data-url="images/parallax1.jpg" data-mobile="true" data-speed="1">
+            <div class="bg-primary well-5 well-5__off">
+
+                <div class="container wow fadeInUp">
+                    <h2><strong>Our </strong>Recommendation to your Business</h2>
+
+                    <p class="offset">We recommend that you view your mobile strategy as a
+movement that can be transformational to your project,
+Company or Organization. It is about discovering how to
+infuse a new medium into your business and build an
+innovative platform that will take you not just into the
+next year, but into the next decade. For example:
+For a sales-based organization, you could use the mobile
+web to get information to your sales people in the field,
+allowing them to bring up live pricing and estimates
+while they are at lunch with a Client.
+For health care, you could use mobile devices to provide
+records; access to formularies, policies, and procedures;
+even patient charts—and all at the patient’s bedside.
+					</p>
+                    <a href="#" class="btn-primary btn-primary__md btn-off">Learn more</a>
+                </div>
+
+            </div>
+        </section>
+        <section class="well-4 well-4__off">
+            <div class="container">
+                <h3><strong>Our Technologies </strong>Supports</h3>
+
+                <div class="row row__off-1">
+                    <div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                            <li>
+                                <a href='javascript:void(0);'>Magento and OpenCart</a>
+                            </li>
+                            <li>
+                                <a href='javascript:void(0);'>SaaS - Shopify, BigCommerce and 3dCart</a>
+                            </li>
+                            <li>
+                                <a href='javascript:void(0);'>CMS - Wordpress and Joomla</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                       		<li>
+                                <a href='javascript:void(0);'>AngularJs, CakePhp, YiiPhp and NodeJS</a>
+                            </li>
+                            <li>
+                                <a href='javascript:void(0);'>Ruby&Rails, Cuba Microframework and Trailblazer</a>
+                            </li>
+                            
+                            <li>
+                                <a href='javascript:void(0);'>Mysql, MongoDB and Oracle</a>
+                            </li>
+                        </ul>
+                    </div>
+					<div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+                        <ul class="marked-list offset-1" style="margin-top:25px;">
+                            <li>
+                                <a href='javascript:void(0);'>Amazon AWS, Cloud VPN and Virtualization
+                                </a>
+                            </li>
+                            <li>
+                                <a href='javascript:void(0);'>Payment gateway - Paypal, Autorized.Net and SecurePay</a>
+                            </li>
+                            <li>
+                                <a href='javascript:void(0);'>POS terminal integration with SaaS Platforms</a>
+                            </li>
+                        </ul>
+                    </div>
+					
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <?php include 'elements/footer.php'; ?>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
